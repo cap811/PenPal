8e179a642a51dd0cd0dbba509573c01791cce909
cap811 <58876414+cap811@users.noreply.github.com>
2022-03-19 23:40:04 +0200

    Add some styles.

--- models/User.js
+++ models/User.js
@@ -1,3 +1,15 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
+const userSchema = new Schema({
+    username: String,
+    email: {
+        type: String,
+        required: true,
+        validate: [ isEmail, 'invalid email']
+    },
+    password: {
+
+    }
+
+});
